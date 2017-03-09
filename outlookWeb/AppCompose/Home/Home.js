bca2c14e376010ae090e262419ba779da7f4578d
ChenWes <chenxuhua0530@163.com>
2017-03-09 12:06:29 +0800

    project init

---
+++ outlookWeb/AppCompose/Home/Home.js
@@ -0,0 +1,41 @@
+﻿/// <reference path="../App.js" />
+
+(function () {
+    'use strict';
+
+    // 每次加载新页面时均必须运行初始化函数
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+
+            $('#set-subject').click(setSubject);
+            $('#get-subject').click(getSubject);
+            $('#add-to-recipients').click(addToRecipients);
+        });
+    };
+
+    function setSubject() {
+        Office.cast.item.toItemCompose(Office.context.mailbox.item).subject.setAsync("Hello world!");
+    }
+
+    function getSubject() {
+        Office.cast.item.toItemCompose(Office.context.mailbox.item).subject.getAsync(function (result) {
+            app.showNotification('当前主题是', result.value)
+        });
+    }
+
+    function addToRecipients() {
+        var item = Office.context.mailbox.item;
+        var addressToAdd = {
+            displayName: Office.context.mailbox.userProfile.displayName,
+            emailAddress: Office.context.mailbox.userProfile.emailAddress
+        };
+ 
+        if (item.itemType === Office.MailboxEnums.ItemType.Message) {
+            Office.cast.item.toMessageCompose(item).to.addAsync([addressToAdd]);
+        } else if (item.itemType === Office.MailboxEnums.ItemType.Appointment) {
+            Office.cast.item.toAppointmentCompose(item).requiredAttendees.addAsync([addressToAdd]);
+        }
+    }
+
+})();
---
+++ outlookWeb/AppRead/Home/Home.js
@@ -0,0 +1,34 @@
+﻿/// <reference path="../App.js" />
+
+(function () {
+    "use strict";
+
+    // 每次加载新页面时均必须运行 Office 初始化函数
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+
+            displayItemDetails();
+        });
+    };
+
+    // 根据当前的邮件项显示“主题”和“发件人”字段
+    function displayItemDetails() {
+        var item = Office.cast.item.toItemRead(Office.context.mailbox.item);
+        $('#subject').text(item.subject);
+
+        var from;
+        if (item.itemType === Office.MailboxEnums.ItemType.Message) {
+            from = Office.cast.item.toMessageRead(item).from;
+        } else if (item.itemType === Office.MailboxEnums.ItemType.Appointment) {
+            from = Office.cast.item.toAppointmentRead(item).organizer;
+        }
+
+        if (from) {
+            $('#from').text(from.displayName);
+            $('#from').click(function () {
+                app.showNotification(from.displayName, from.emailAddress);
+            });
+        }
+    }
+})();
