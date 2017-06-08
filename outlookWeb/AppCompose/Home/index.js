1f43c036a2a9a498ca32d9fde568ab632a7a20f6
ChenWes <chenxuhua0530@163.com>
2017-06-08 15:05:05 +0800

    get data success

--- outlookWeb/AppCompose/Home/index.js
+++ outlookWeb/AppCompose/Home/index.js
@@ -1,251 +1,27 @@
-﻿var ngapp = angular.module('ngapp', ['ngMaterial', 'angularGrid']);
+﻿var ngapp = angular.module('ngapp', ['ngMaterial', 'angularGrid', 'AdalAngular'])
+    .config(['$httpProvider', 'adalAuthenticationServiceProvider', function ($httpProvider, adalProvider) {
+        adalProvider.init(
+            {
+                instance: 'https://login.microsoftonline.com/',
+                tenant: '29abf16e-95a2-4d13-8d51-6db1b775d45b',
+                clientId: '741a869c-ce4c-46c0-8794-60a6391293ca',
+                extraQueryParameter: 'nux=1',
+                cacheLocation: 'localStorage' // enable this for IE, as sessionStorage does not work for localhost.
+            },
+            $httpProvider
+        );
 
-ngapp.controller('MainCtrl', function ($scope, $mdToast, $http, $q) {
+    }]);;
+
+ngapp.controller('MainCtrl', ['$scope', '$mdToast', '$http', 'adalAuthenticationService', '$q', function ($scope, $mdToast, $http, adalService, $q) {
     var vm = this;
 
-    $scope.yarns = [
-        { id: '', lable: 'ALL' },
-        { id: '1', lable: '(80/2)/2' },
-        { id: '2', lable: '10' },
-        { id: '3', lable: '10/10' },
-        { id: '4', lable: '10/2' },
-        { id: '5', lable: '10/20' },
-        { id: '6', lable: '10/3' },
-        { id: '7', lable: '10/30' },
-        { id: '8', lable: '10/5' },
-        { id: '9', lable: '10\40D' },
-        { id: '10', lable: '10\70D' },
-        { id: '11', lable: '100' },
-        { id: '12', lable: '100/100' },
-        { id: '13', lable: '100/2' },
-        { id: '14', lable: '100/3' },
-        { id: '15', lable: '1000D' },
-        { id: '16', lable: '100D' },
-        { id: '17', lable: '100D/144F' },
-        { id: '18', lable: '100D/288F' },
-        { id: '19', lable: '100D/32FT80' },
-        { id: '20', lable: '100D/36F' },
-        { id: '21', lable: '100D/48F' },
-        { id: '22', lable: '100DT400' },
-        { id: '23', lable: '11/3' },
-        { id: '24', lable: '110D' },
-        { id: '25', lable: '115D' },
-        { id: '26', lable: '12' },
-        { id: '27', lable: '12/12' },
-        { id: '28', lable: '12/2' },
-        { id: '29', lable: '12/3' },
-        { id: '30', lable: '12/30' },
-        { id: '31', lable: '120' },
-        { id: '32', lable: '120/2' },
-        { id: '33', lable: '120/3' },
-        { id: '34', lable: '120D' },
-        { id: '35', lable: '124/2' },
-        { id: '36', lable: '128D' },
-        { id: '37', lable: '129D' },
-        { id: '38', lable: '12B' },
-        { id: '39', lable: '1300D' },
-        { id: '40', lable: '130D' },
-        { id: '41', lable: '130D/96F' },
-        { id: '42', lable: '133D' },
-        { id: '43', lable: '135D' },
-        { id: '44', lable: '138D' },
-        { id: '45', lable: '14' },
-        { id: '46', lable: '14/14' },
-        { id: '47', lable: '14/2' },
-        { id: '48', lable: '14/32' },
-        { id: '49', lable: '140/2' },
-        { id: '50', lable: '140D' },
-        { id: '51', lable: '140D/48F' },
-        { id: '52', lable: '15\70D' },
-        { id: '53', lable: '150D' },
-        { id: '54', lable: '150D/144F' },
-        { id: '55', lable: '150D/2' },
-        { id: '56', lable: '155D' },
-        { id: '57', lable: '16' },
-        { id: '58', lable: '16/16' },
-        { id: '59', lable: '16/2' },
-        { id: '60', lable: '16/40' },
-        { id: '61', lable: '16\40D' },
-        { id: '62', lable: '16\70D' },
-        { id: '63', lable: '160D' },
-        { id: '64', lable: '17' },
-        { id: '65', lable: '170/2' },
-        { id: '66', lable: '18' },
-        { id: '67', lable: '20' },
-        { id: '68', lable: '20/2' },
-        { id: '69', lable: '20/20' },
-        { id: '70', lable: '20/20\70D' },
-        { id: '71', lable: '20/3' },
-        { id: '72', lable: '20/32' },
-        { id: '73', lable: '20/40' },
-        { id: '74', lable: '20/60' },
-        { id: '75', lable: '20/8' },
-        { id: '76', lable: '20\40D' },
-        { id: '77', lable: '20\70D' },
-        { id: '78', lable: '200/2' },
-        { id: '79', lable: '200D' },
-        { id: '80', lable: '20D\20D' },
-        { id: '81', lable: '21' },
-        { id: '82', lable: '21/2' },
-        { id: '83', lable: '21\40D' },
-        { id: '84', lable: '222D' },
-        { id: '85', lable: '23' },
-        { id: '86', lable: '24' },
-        { id: '87', lable: '24/2' },
-        { id: '88', lable: '24/30' },
-        { id: '89', lable: '240D' },
-        { id: '90', lable: '25' },
-        { id: '91', lable: '25/2' },
-        { id: '92', lable: '25/25' },
-        { id: '93', lable: '25/50' },
-        { id: '94', lable: '26' },
-        { id: '95', lable: '28' },
-        { id: '96', lable: '290D' },
-        { id: '97', lable: '30' },
-        { id: '98', lable: '30/2' },
-        { id: '99', lable: '30/20' },
-        { id: '100', lable: '30/3' },
-        { id: '101', lable: '30/30' },
-        { id: '102', lable: '30/50' },
-        { id: '103', lable: '30\30DT400' },
-        { id: '104', lable: '30\40D' },
-        { id: '105', lable: '30\42DX' },
-        { id: '106', lable: '30\70D' },
-        { id: '107', lable: '300/3' },
-        { id: '108', lable: '300D' },
-        { id: '109', lable: '305/3' },
-        { id: '110', lable: '30D' },
-        { id: '111', lable: '32' },
-        { id: '112', lable: '32/2' },
-        { id: '113', lable: '32/3' },
-        { id: '114', lable: '32/32' },
-        { id: '115', lable: '32\68D' },
-        { id: '116', lable: '32+68D' },
-        { id: '117', lable: '320/4' },
-        { id: '118', lable: '325D' },
-        { id: '119', lable: '330/3' },
-        { id: '120', lable: '34' },
-        { id: '121', lable: '34D/6F' },
-        { id: '122', lable: '35' },
-        { id: '123', lable: '35/2' },
-        { id: '124', lable: '36' },
-        { id: '125', lable: '36/2' },
-        { id: '126', lable: '4.5' },
-        { id: '127', lable: '40' },
-        { id: '128', lable: '40/2' },
-        { id: '129', lable: '40/20' },
-        { id: '130', lable: '40/3' },
-        { id: '131', lable: '40/30' },
-        { id: '132', lable: '40/40' },
-        { id: '133', lable: '40/40\40D' },
-        { id: '134', lable: '40/50' },
-        { id: '135', lable: '40/60' },
-        { id: '136', lable: '40\20D' },
-        { id: '137', lable: '40\30DX' },
-        { id: '138', lable: '40\40D' },
-        { id: '139', lable: '40\40D/2' },
-        { id: '140', lable: '40\40D/40\40D' },
-        { id: '141', lable: '40\40DX' },
-        { id: '142', lable: '40\42D' },
-        { id: '143', lable: '40\42DX' },
-        { id: '144', lable: '40\55DX' },
-        { id: '145', lable: '40\70D' },
-        { id: '146', lable: '40\70DX' },
-        { id: '147', lable: '40\75D' },
-        { id: '148', lable: '40\75DX' },
-        { id: '149', lable: '40+40D' },
-        { id: '150', lable: '40+70D' },
-        { id: '151', lable: '40D/12F' },
-        { id: '152', lable: '40D/34F' },
-        { id: '153', lable: '40D\20D' },
-        { id: '154', lable: '44' },
-        { id: '155', lable: '44/2' },
-        { id: '156', lable: '45' },
-        { id: '157', lable: '5' },
-        { id: '158', lable: '5.5' },
-        { id: '159', lable: '50' },
-        { id: '160', lable: '50/2' },
-        { id: '161', lable: '50/20' },
-        { id: '162', lable: '50/3' },
-        { id: '163', lable: '50/30' },
-        { id: '164', lable: '50/50' },
-        { id: '165', lable: '50\20D' },
-        { id: '166', lable: '50\30D' },
-        { id: '167', lable: '50\30DX' },
-        { id: '168', lable: '50\40D' },
-        { id: '169', lable: '50\40D/50' },
-        { id: '170', lable: '50\40DX' },
-        { id: '171', lable: '50\42D' },
-        { id: '172', lable: '50\42DX' },
-        { id: '173', lable: '50\70D' },
-        { id: '174', lable: '50+100/2' },
-        { id: '175', lable: '50+40D' },
-        { id: '176', lable: '500D' },
-        { id: '177', lable: '50D' },
-        { id: '178', lable: '50D/2' },
-        { id: '179', lable: '50D/3F' },
-        { id: '180', lable: '52/2' },
-        { id: '181', lable: '6' },
-        { id: '182', lable: '6/6' },
-        { id: '183', lable: '60' },
-        { id: '184', lable: '60/100' },
-        { id: '185', lable: '60/2' },
-        { id: '186', lable: '60/3' },
-        { id: '187', lable: '60/30' },
-        { id: '188', lable: '60/6' },
-        { id: '189', lable: '60/60' },
-        { id: '190', lable: '60/60\30D' },
-        { id: '191', lable: '60/80' },
-        { id: '192', lable: '60\30D' },
-        { id: '193', lable: '60\30D/2' },
-        { id: '194', lable: '60\30D/60\30D' },
-        { id: '195', lable: '60\30DX' },
-        { id: '196', lable: '60\40D' },
-        { id: '197', lable: '60\40DX' },
-        { id: '198', lable: '60+120/2' },
-        { id: '199', lable: '60D' },
-        { id: '200', lable: '60D/3F' },
-        { id: '201', lable: '62' },
-        { id: '202', lable: '62/2' },
-        { id: '203', lable: '67D' },
-        { id: '204', lable: '7' },
-        { id: '205', lable: '7/2' },
-        { id: '206', lable: '70' },
-        { id: '207', lable: '70/2' },
-        { id: '208', lable: '70/70' },
-        { id: '209', lable: '700/8' },
-        { id: '210', lable: '70D\20D' },
-        { id: '211', lable: '74/2' },
-        { id: '212', lable: '75D' },
-        { id: '213', lable: '75D/72F' },
-        { id: '214', lable: '75DT400' },
-        { id: '215', lable: '76D' },
-        { id: '216', lable: '8' },
-        { id: '217', lable: '8/8' },
-        { id: '218', lable: '80' },
-        { id: '219', lable: '80/2' },
-        { id: '220', lable: '80/2\40D' },
-        { id: '221', lable: '80/2+40D' },
-        { id: '222', lable: '80/30' },
-        { id: '223', lable: '80/32' },
-        { id: '224', lable: '80/40' },
-        { id: '225', lable: '80/50' },
-        { id: '226', lable: '80/75DT400' },
-        { id: '227', lable: '80/80' },
-        { id: '228', lable: '80\20D' },
-        { id: '229', lable: '80\20D/2' },
-        { id: '230', lable: '80\20D/2/50' },
-        { id: '231', lable: '80\20D/80' },
-        { id: '232', lable: '80\20D/80\20D' },
-        { id: '233', lable: '80\20DX/2' },
-        { id: '234', lable: '83/2' },
-        { id: '235', lable: '85/2' },
-        { id: '236', lable: '87D' },
-        { id: '237', lable: '90' },
-        { id: '238', lable: '90/2' },
-        { id: '239', lable: '95D' },
-        { id: '240', lable: '97D' }
-    ];
+    $scope.login = function () {
+        adalService.login();
+    };
+    $scope.logout = function () {
+        adalService.logOut();
+    };
 
     $scope.card = {};
     $scope.card.title = 'Esquel';
@@ -271,8 +47,25 @@ ngapp.controller('MainCtrl', function ($scope, $mdToast, $http, $q) {
         vm.styles = [];
         vm.loadingMore = false;
 
-        vm.loadMoreShots();
-        $(".angular-grid-item").css("position", "relative");
+        if (adalService.userInfo) {
+            var resource = 'https://esquel.onmicrosoft.com/705cadd7-d8b2-44f7-9c28-3841c112f04b';
+            var oldtoken = adalService.getCachedToken(resource);
+            if (!oldtoken) {
+                adalService.acquireToken(resource).then(function (token) {
+                    vm.loadMoreShots(token);
+                    $(".angular-grid-item").css("position", "relative");
+                }).catch(function (err) {
+                    showError(err);
+                });
+            }
+            else {
+                vm.loadMoreShots(oldtoken);
+                $(".angular-grid-item").css("position", "relative");
+            }
+        } else {
+            showError('no userinfo ,please login first');
+        }
+
     }
 
     /* copy content to mail */
@@ -282,7 +75,7 @@ ngapp.controller('MainCtrl', function ($scope, $mdToast, $http, $q) {
         var ct = $("#dv_" + dvId);
 
         var item = Office.cast.item.toItemCompose(Office.context.mailbox.item);
-        
+
         item.body.getTypeAsync(
             function (result) {
                 if (result.status != Office.AsyncResultStatus.Failed) {
@@ -323,7 +116,7 @@ ngapp.controller('MainCtrl', function ($scope, $mdToast, $http, $q) {
     }
 
     /* call WebAPI function */
-    vm.loadMoreShots = function () {
+    vm.loadMoreShots = function (token) {
 
         if (vm.loadingMore) return;
 
@@ -331,7 +124,7 @@ ngapp.controller('MainCtrl', function ($scope, $mdToast, $http, $q) {
         vm.loadingMore = true;
 
         var promise;
-        var wsUrl = 'https://dev-esb.esquel.com:6114/sprintwes/api/v1/styleproduct/' + vm.pagesize + '/' + vm.page;
+        var wsUrl = 'https://designer-workbench-dev.azurewebsites.net/api/v1/styleproduct/' + vm.pagesize + '/' + vm.page;
         var soapRequest = '';
 
         if ($scope.SearchInfo.styleID != '') {
@@ -349,13 +142,17 @@ ngapp.controller('MainCtrl', function ($scope, $mdToast, $http, $q) {
             soapRequest = {};
         }
 
+        console.log('token------------');
+        console.log(token);
+
         var req = {
             method: 'POST',
             url: wsUrl,
             data: JSON.stringify(soapRequest),
             headers: {
                 "Content-Type": "application/json",
-                "Accept": "application/json"
+                "Accept": "application/json",
+                "Authorization": "Bearer " + token
             },
             cache: false
         }
@@ -375,7 +172,7 @@ ngapp.controller('MainCtrl', function ($scope, $mdToast, $http, $q) {
                         if (!vm.styles[0]) {
                             showError('No Style Mathced.');
                         } else {
-                            showInfo('We Found Page [' + vm.page + '] ' + vm.styles.length + ' Style.');
+                            showInfo('We Found Page [' + vm.page + '] ' + vm.styles.length + ' Style.' + token);
 
                             //check next page
                             if (vm.page < totalPage) {
@@ -419,7 +216,7 @@ ngapp.controller('MainCtrl', function ($scope, $mdToast, $http, $q) {
                 .hideDelay(5000)
         );
     }
-});
+}]);
 
 
 ngapp.filter('unsafe', function ($sce) { return $sce.trustAsHtml; });
